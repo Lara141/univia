608977c13fed714ebe343281de6013f7575fb7c5
Valentina-Benitez <valenbenitez246@gmail.com>
2026-04-22 14:05:58 -0300

    BaseController ya está documentado en español.

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,41 +7,24 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
-/**
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- *
- * Extend this class in any new controllers:
- * ```
- *     class Home extends BaseController
- * ```
- *
- * For security, be sure to declare any new methods as protected or private.
- */
+/*
+Clase base abstracta de la cual heredan todos los controladores de la aplicación. 
+Centraliza configuraciones comunes para evitar repetir código.
+*/
+
 abstract class BaseController extends Controller
 {
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-
-    // protected $session;
+    /*Define los helpers que se cargan automáticamente en todos los controladores que extienden esta clase.*/
     protected $helpers = ['form', 'url', 'session'];
 
-    /**
-     * @return void
-     */
+    /* 
+    Método que se ejecuta automáticamente al inicializar cualquier controlador que herede de BaseController.
+    */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-        // Load here all helpers you want to be available in your controllers that extend BaseController.
-        // Caution: Do not put the this below the parent::initController() call below.
-        // $this->helpers = ['form', 'url'];
-
-        // Caution: Do not edit this line.
+       
         parent::initController($request, $response, $logger);
 
-        // Preload any models, libraries, etc, here.
-        // $this->session = service('session');
     }
 
 }
